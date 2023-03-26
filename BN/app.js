12ea81320b1269a74dfd14e8f659bad081638bd9
babith02 <116098898+babith02@users.noreply.github.com>
2023-03-26 12:58:53 +0530

    Backend

---
+++ BN/app.js
@@ -0,0 +1,105 @@
+require("dotenv").config();
+require('./config/db').connect();
+const express = require("express");
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.post("/register", async (req, res) => {
+
+    try {
+        // Get user input
+        const { name, email, password } = req.body;
+        
+        // Validate user input
+        if (!(name && email && password)) {
+            res.status(400).send("All input is required");
+        }
+
+        // check if user already exist
+        // Validate if user exist in our database
+        const oldUser = await User.findOne({ email });
+
+        if (oldUser) {
+          res.json("User Already Exist. Please Login");
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+
+        //Encrypt user password
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        // Create user in our database
+        const user = await User.create({
+            name,
+            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword,
+        });
+
+        // Create token
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            "babith_token",
+            {
+                expiresIn: "2h",
+            }
+        );
+        // save user token
+        user.token = token;
+
+        // return new user
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+app.post("/login", async (req, res) => {
+
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        "babith_token",
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(402).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
+
+const auth = require("./middleware/auth");
+
+app.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+
+module.exports = app;
